dfa77b012704cc83e8c7d7cd4c62b5274398e63c
syedmuhammadhussain <muhammadhussain@folio3.com>
2021-11-12 17:57:54 +0500

    added car class

--- problem-2.py
+++ problem-2.py
@@ -6,3 +6,11 @@ def accelerate(car):
             car.gearUp()
     elif car.getTransmission() == "Manual":
         car.increaseSpeedBy(10)
+
+
+class Car:
+    def __init__(self, maxSpeed, transmission, speed=0, gear=1):
+        self.maxSpeed = maxSpeed
+        self.transmission = transmission
+        self.speed = speed
+        self.gear = gear
